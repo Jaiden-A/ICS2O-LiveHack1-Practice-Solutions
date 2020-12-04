69e248722964bd6285f248cb86375f2e46add902
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2020-12-04 14:22:44 +0000

    complete f_to_c

--- f_to_c.py
+++ f_to_c.py
@@ -1,1 +1,23 @@
+'''
+-------------------------------------------------------------------------------
+Name:		f_to_c.py
+Purpose:	Write a program that lets you enter a degree measure in Fahrenheit 
+and prints the result in celsius degree measure.
+
+Author:	Fabroa.E
+
+Created:	date in 04/12/2020
+------------------------------------------------------------------------------
+'''
+
+print("****** Fahrenheit to Celsius Converter ******")
+
+# get Fahrenheit from user
+temp_f = float(input("Enter the temperature in Fahrenheit: "))
+
+# compute celsius
+temp_c = (5/9) * (temp_f - 32)  
+
+# output celsius
+print("The temperature in Celsius is " + str(temp_c) + "°.")
 
